9aac279efaaf5e318663891e88589aec3128d2e6
xuepeng.zhang <xuepeng.zhang@ecovacs.com>
2022-08-26 08:50:26 +0800

    modify my_picture_write.cpp

--- src/mine_picture/src/my_picture_write.cpp
+++ src/mine_picture/src/my_picture_write.cpp
@@ -4,23 +4,22 @@
 #include <opencv2/highgui.hpp>
 #include <cv_bridge/cv_bridge.h>
 #include <string>
-static int i = 0;
+static int Picture_index = 0;
 void mpic_callback(const sensor_msgs::ImageConstPtr &img_msg)
 {
-     ros::Time time = img_msg->header.stamp;
      std::string str;
-     str = "/home/zxp/Pictures/cpjpg/" + std::to_string(i) + ".jpg";
+     str = "/home/zxp/Pictures/cpjpg/" + std::to_string(Picture_index) + ".jpg";
      cv_bridge::CvImageConstPtr ptr;
      ptr = cv_bridge::toCvCopy(img_msg, "bgr8");
-     printf("%s\n", str.c_str());
      cv::imwrite(str, ptr->image);
-     i++;
+     Picture_index++;
+     if(Picture_index>50)Picture_index=0;
 }
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "mpic_save");
-  ros::NodeHandle n;
-  ros::Subscriber sub_image = n.subscribe("mpic/camera/image", 1, mpic_callback);   
+  ros::NodeHandle nh;
+  ros::Subscriber sub_image = nh.subscribe("mpic/camera/image", 1, mpic_callback);   
   ros::spin();
   return 0;
 }
